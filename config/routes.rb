cc66a7e9506106b227ef1e5d9023172723254636
Taylor Huston <taylor.a.huston@gmail.com>
2015-11-29 22:07:27 -0700

    Set up Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
+  root to: "books#index"
 
   resources :books do
     resources :notes, only: [:create, :destroy]
   end
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
     
-  root to: "books#index"
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
